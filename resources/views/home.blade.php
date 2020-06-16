4703d45b6169f313ffaf262a0221d7f73cf11e07
Matteo Francesco Leuterio <leuterio.matteo@gmail.com>
2020-06-16 15:29:06 +0200

    migration done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Boolean Classes</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+      
+    </head>
+    <body>
+        <header class="main-header">
+            <h1>Boolean Classes</h1>
+        </header>
+
+        <main class="main-content">
+           
+        </main>
+
+        <footer class="main-footer">
+            footer
+        </footer>
+    </body>
+</html>
